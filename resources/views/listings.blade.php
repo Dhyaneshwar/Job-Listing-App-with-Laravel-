b47e12be47287db1ccf47d38b06b3f381565a7c1
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2024-02-05 20:09:21 +0000

    feat: adding the theme for the listings

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,8 +1,10 @@
 @extends('layout')
 
-@section('contentssss')
-    
-<h1>{{$heading}}</h1>
+@section('content')
+@include('partial._hero')
+@include('partial._search')
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
     {{--
         We can do the listing check using the if condition too.
         @if (count($listings) == 0)
@@ -12,15 +14,40 @@
 
     @unless(count($listings) == 0)
         @foreach($listings as $listing)
-            <h2>
-                <a href='/listings/{{$listing['id']}}'>{{$listing['title']}}</a>
-            </h2>
-            <p>{{$listing['description']}}</p>
+        <div class="bg-gray-50 border border-gray-200 rounded p-6">
+            <div class="flex">
+                <img
+                    class="hidden w-48 mr-6 md:block"
+                    src="{{asset('images/no-image.png')}}"
+                    alt=""
+                />
+                <div>
+                    <h3 class="text-2xl">
+                        <a href='/listings/{{$listing->id}}'>{{$listing['title']}}</a>
+                    </h3>
+                    <div class="text-xl font-bold mb-4">{{$listing['company']}}</div>
+                    <h1>{{$listing['tags']}}</h1>
+                    <?php print_r($listing['tags']) ?>
+                    {{-- <ul class="flex">
+                        @foreach($listing['tags'] as $tag)
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                            >
+                                <a href="#">{{$tag}}</a>
+                            </li>
+                        @endforeach
+                    </ul> --}}
+                    <div class="text-lg mt-4">
+                        <i class="fa-solid fa-location-dot"></i> {{$listing['location']}}
+                    </div>
+                </div>
+            </div>
+        </div>
         @endforeach
 
     @else
         <p>No Listing Found</p>
 
     @endunless
-
+</div>
 @endsection
