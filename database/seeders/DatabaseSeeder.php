fda6a9651f8c6e5cdad5e8e5717d601e209f8f96
Dhyaneshwar N <dhyaneshwar98@gmail.com>
2024-02-05 19:27:53 +0000

    feat: create a new factory for listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,21 +12,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(4)->create();
 
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        Listing::create([
-            'title' => 'Laravel Senior Develor',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'email1@email.com',
-            'website' => 'https://www.acme.com',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacinia at quis risus sed vulputate odio. In hac habitasse platea dictumst quisque sagittis purus sit amet. Tincidunt dui ut ornare lectus sit amet. Fermentum dui faucibus in ornare quam viverra orci sagittis eu.'
-        ]);
+
+        Listing::factory(4)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacinia at quis risus sed vulputate odio. In hac habitasse platea dictumst quisque sagittis purus sit amet. Tincidunt dui ut ornare lectus sit amet. Fermentum dui faucibus in ornare quam viverra orci sagittis eu.'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backward, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://www.starkindustries.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est ultricies integer quis auctor elit sed. Iaculis at erat pellentesque adipiscing commodo elit at. Sem fringilla ut morbi tincidunt augue interdum velit. Commodo odio aenean sed adipiscing.'
+        // ]);
     }
 }
